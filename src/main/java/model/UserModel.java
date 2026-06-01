30403cd4bec86aa7349e409071e886a665ed2bb2
Jose Manuel Camacho <camachocastelanjm@gmail.com>
2026-05-31 23:45:51 -0600

    feat: modelo de usuario para usarlo en interfaz (se me olvido ponerlo como staged para usarlo como JB)

--- src/main/java/model/UserModel.java
+++ src/main/java/model/UserModel.java
@@ -1,5 +1,46 @@
 package model;
 
-public class UserModel {
+import java.io.Serializable;
+import java.util.List;
+
+public class UserModel implements Serializable{
+    private String IDUser;
+    private String emailUser;
+    private String nameUser;
+    private List<PracticeModel> Practices;
     
+    public UserModel() {
+    }
+
+    public String getIDUser() {
+        return IDUser;
+    }
+
+    public void setIDUser(String iDUser) {
+        IDUser = iDUser;
+    }
+
+    public String getEmailUser() {
+        return emailUser;
+    }
+
+    public void setEmailUser(String emailUser) {
+        this.emailUser = emailUser;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public List<PracticeModel> getPractices() {
+        return Practices;
+    }
+
+    public void setPractices(List<PracticeModel> practices) {
+        Practices = practices;
+    }
 }
