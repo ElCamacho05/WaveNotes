311db3f98907bca98471ff3f009d4934a9a0b127
Jose Manuel Camacho <camachocastelanjm@gmail.com>
2026-06-05 01:31:37 -0600

    feat: mejoras en algoritmo de deteccion de notas de tambores de bateria

--- musicApp/instruments/specific/drums.py
+++ musicApp/instruments/specific/drums.py
@@ -3,6 +3,7 @@ import numpy as np
 import tempfile
 import os
 from fastapi import Request, HTTPException
+import logging
 
 
 async def extract_drums(request: Request):
@@ -26,17 +27,47 @@ async def extract_drums(request: Request):
         y, sr = librosa.load(temp_path, sr=None)
 
         # deteccion del onset de los golpes a la bateria
-        # detecta los momentos de mayor ruido en el auido de la bateria
-        # para retornarlos
-        beat_moments = librosa.onset.onset_detect(y=y, sr=sr, units='time')
+        # detecta los momentos de mayor ruido en el auido de la bateria para retornarlos
+        beat_frames = librosa.onset.onset_detect(y=y, sr=sr, units='frames')
+        
+        # convertir frames a segundos exactos
+        beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 
-        final_beats = beat_moments.tolist()
+        # espectrograma de las notas
+        S = np.abs(librosa.stft(y))
 
-        print(f"!! Extracción completada: {len(final_beats)} golpes de batería detectados")
+        freqs = librosa.fft_freqs(sr=sr)
 
-        return {"status": "success", "notas": final_beats}
+        low_band = (freqs > 20) & (freqs < 150)     # rango del Bombo
+        mid_band = (freqs > 150) & (freqs < 2000)   # rango de Tarola
+        high_band = (freqs > 4000)                        # rango de Platillos
+
+        clasif = []
+
+        for i, frame in enumerate(beat_frames):
+            ie = S[:, frame]
+            
+            e_bass_drum = np.sum(ie[low_band])          # bombo
+            e_snare = np.sum(ie[mid_band])              # caja/tarola
+            e_hi_hat = np.sum(ie[high_band])            # platillos
+            
+            if e_bass_drum > energia_tarola and e_bass_drum > e_hi_hat:
+                tipo = 0 # KICK
+            elif e_hi_hat > e_bass_drum and e_hi_hat > energia_tarola:
+                tipo = 2 # HIHAT
+            else:
+                tipo = 1 # SNARE
+                
+            rt_seconds = float(beat_times[i])
+            clasif.append({"targetDrum": tipo, "time": rt_seconds})
+        
+        print(f"!! Extracción completada: {len(clasif)} golpes de batería detectados")
+
+        return {"status": "success", "notas": clasif}
     except Exception as e:
+        logging.exception("An error occurred during calculation")
         raise HTTPException(status_code=500, detail=str(e))
     finally:
+        print()
         if os.path.exists(temp_path):
             os.remove(temp_path)
